dc28d90496821946ac85baad8ac26d0fa40d0e32
Onur Şavkı <42342278+onursavki@users.noreply.github.com>
2025-04-27 22:33:43 +0300

    system-and-equipment.js

--- system-and-equipment.js
+++ system-and-equipment.js
@@ -241,7 +241,7 @@ systemData.forEach(item => {
   const description = checkDescription(item.description); // "Takılı Değil" durumu ve parantez içi kontrolü
 
   // Eğer img alanı boşsa, https://i.hizliresim.com/1i2vije.png linkini ekle
-  const imgSrc = item.img && item.img.trim() !== "" ? item.img : "https://i.hizliresim.com/1i2vije.png";
+  const imgSrc = item.img && item.img.trim() !== "" ? item.img : "https://i.hizliresim.com/7ttnp7e.png";
 
   systemTable.innerHTML += `
     <tr>
@@ -261,7 +261,7 @@ equipmentData.forEach(item => {
   const description = checkDescription(item.description); // "Takılı Değil" durumu ve parantez içi kontrolü
 
   // Eğer img alanı boşsa, https://i.hizliresim.com/1i2vije.png linkini ekle
-  const imgSrc = item.img && item.img.trim() !== "" ? item.img : "https://i.hizliresim.com/1i2vije.png";
+  const imgSrc = item.img && item.img.trim() !== "" ? item.img : "https://i.hizliresim.com/7ttnp7e.png";
 
   equipmentTable.innerHTML += `
     <tr>
